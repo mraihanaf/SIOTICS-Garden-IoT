4595a67a560b8246b8e52bf37d984744b5dd7c99
mraihanaf <mraihanaf0@gmail.com>
2024-11-04 01:42:14 +0700

    test(server): add mqtt esp32 client mocker

---
+++ apps/server/src/__test__/mqttclient.ts
@@ -0,0 +1,76 @@
+import { connectAsync, MqttClient } from "mqtt"
+import { config } from "dotenv"
+import Pino, { Logger } from "pino"
+
+config()
+const logger: Logger = Pino({
+    name: "SiramPaksa-ESP32-Mocker",
+    level: process.env.LOG_LEVEL || "debug",
+})
+let heartbeatIntervarId: null | NodeJS.Timeout = null
+let cronExpression = ""
+let wateringDurationInMs = 0
+
+function handleDisconnected(client: MqttClient) {
+    logger.info("Client disconnected, reconnecting...")
+    client.end()
+    if (heartbeatIntervarId) clearInterval(heartbeatIntervarId)
+    clientStart()
+}
+
+function sendHeartbeat(client: MqttClient): void {
+    logger.debug("sending heartbeat")
+    if (!client.connected) return handleDisconnected(client)
+    client.publish(
+        `esp/${client.options.clientId}/heartbeat`,
+        JSON.stringify({
+            date: new Date().toISOString(),
+            wateringDurationInMs: wateringDurationInMs,
+            cronExp: cronExpression,
+        }),
+        { qos: 0 },
+        (err) => {},
+    )
+}
+
+async function clientStart() {
+    try {
+        const client: MqttClient = await connectAsync(
+            `mqtt://localhost:${process.env.MQTT_PORT}`,
+            {
+                clientId: "test",
+            },
+        )
+        logger.info("connected to the broker")
+        client.publish("esp/init", client.options.clientId!, { qos: 2 })
+        client.subscribe(`esp/${client.options.clientId}/watering/setCron`)
+        client.subscribe(
+            `esp/${client.options.clientId}/watering/setDurationInMs`,
+        )
+
+        client.on("message", (topic, payload) => {
+            if (
+                topic ===
+                `esp/${client.options.clientId}/watering/setDurationInMs`
+            ) {
+                logger.info(`got setDuration message ${payload.toString()}`)
+                wateringDurationInMs = parseInt(payload.toString())
+            } else if (
+                topic === `esp/${client.options.clientId}/watering/setCron`
+            ) {
+                logger.info(`got setCron message ${payload.toString()}`)
+                cronExpression = payload.toString()
+            }
+        })
+        // client.publish("esp/test/system/logs", "berhasil yey")
+        heartbeatIntervarId = setInterval(() => sendHeartbeat(client), 1000)
+    } catch (err) {
+        logger.error(err)
+        logger.error(
+            "failed connect to the broker. attemping reconnection in 3 seconds",
+        )
+        setTimeout(clientStart, 3000)
+    }
+}
+
+setTimeout(clientStart, 3000)
