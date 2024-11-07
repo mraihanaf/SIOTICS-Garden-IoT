cc10f6c5241e12ab5eac80e77d5d7eb619a6b75d
mraihanaf <mraihanaf0@gmail.com>
2024-11-08 05:09:38 +0700

    test(server): update mqtttclient test

--- apps/server/src/__test__/mqttclient.ts
+++ apps/server/src/__test__/mqttclient.ts
@@ -1,6 +1,7 @@
 import { connectAsync, MqttClient } from "mqtt"
 import { config } from "dotenv"
 import Pino, { Logger } from "pino"
+import { CronJob } from "cron"
 
 config()
 const logger: Logger = Pino({
@@ -10,6 +11,8 @@ const logger: Logger = Pino({
 let heartbeatIntervarId: null | NodeJS.Timeout = null
 let cronExpression = ""
 let wateringDurationInMs = 0
+let relayState = true // nyala == tutup keran
+let cronJob: CronJob | null = null
 
 function handleDisconnected(client: MqttClient) {
     logger.info("Client disconnected, reconnecting...")
@@ -27,9 +30,45 @@ function sendHeartbeat(client: MqttClient): void {
             date: new Date().toISOString(),
             wateringDurationInMs: wateringDurationInMs,
             cronExp: cronExpression,
+            isWatering: !relayState, // jika relay nyala maka keran mati, jika relay mati maka keran nyala
         }),
         { qos: 0 },
         (err) => {},
+    )
+}
+
+function changeJob(
+    cronExpression: string,
+    wateringDurationInMs: number,
+    client: MqttClient,
+) {
+    if (cronJob) cronJob.stop()
+    cronJob = new CronJob(
+        cronExpression,
+        () => {
+            // matikan relay (menyiram)
+            logger.info(
+                `matikan relay (menyiram).. selama ${wateringDurationInMs}ms`,
+            )
+            relayState = false
+            client.publish(
+                `esp/${client.options.clientId}/watering/trigger`,
+                "off",
+            )
+            setTimeout(() => {
+                logger.info(
+                    `nyalakan relay, selesai menyiram selama ${wateringDurationInMs}`,
+                )
+                relayState = true
+                client.publish(
+                    `esp/${client.options.clientId}/watering/trigger`,
+                    "on",
+                )
+            }, wateringDurationInMs)
+        },
+        null,
+        true,
+        "Asia/Jakarta",
     )
 }
 
@@ -38,7 +77,7 @@ async function clientStart() {
         const client: MqttClient = await connectAsync(
             `mqtt://localhost:${process.env.MQTT_PORT}`,
             {
-                clientId: "test",
+                clientId: `test`,
             },
         )
         logger.info("connected to the broker")
@@ -55,18 +94,24 @@ async function clientStart() {
             ) {
                 logger.info(`got setDuration message ${payload.toString()}`)
                 wateringDurationInMs = parseInt(payload.toString())
+                changeJob(cronExpression, wateringDurationInMs, client)
             } else if (
                 topic === `esp/${client.options.clientId}/watering/setCron`
             ) {
                 logger.info(`got setCron message ${payload.toString()}`)
                 cronExpression = payload.toString()
+                changeJob(cronExpression, wateringDurationInMs, client)
             } else if (
                 topic === `esp/${client.options.clientId}/watering/trigger`
             ) {
                 if (payload.toString() === "on") {
-                    logger.info("turning on the relay")
+                    logger.info("turning on the relay (berhentikan penyiraman)")
+                    relayState = true
                 } else {
-                    logger.info("turning off the relay")
+                    logger.info(
+                        "turning off the relay (matikan relay/mulai penyiram)",
+                    )
+                    relayState = false
                 }
             }
         })
